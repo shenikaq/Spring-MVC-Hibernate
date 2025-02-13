b87ed517b566637bdd819c287e6aa92bdea9c32c
Дарья <ms@dkorsakova.ru>
2025-02-13 12:44:48 +0300

    finish

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -19,17 +19,7 @@ public class CarController {
     private CarService carService;
 
     @GetMapping(value = "/cars")
-//    public String printCar() {
     public String printCar(@RequestParam(value = "count") Optional<Integer> count, Model model) {
-//        List<Car> cars = new ArrayList<>();
-//        cars.add(new Car("model1", 1, "user1", 2021));
-//        cars.add(new Car("model2", 2, "user2", 2022));
-//        cars.add(new Car("model3", 3, "user3", 2023));
-//        cars.add(new Car("model4", 4, "user4", 2024));
-//        cars.add(new Car("model5", 5, "user5", 2025));
-
-//        model.addAttribute("cars: ", cars);
-
         model.addAttribute("cars", carService.listCars(count.orElse(0)));
         return "cars";
     }
--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -23,12 +23,6 @@ public class Car {
                 ", user='" + user + '\'' +
                 ", year=" + year +
                 '}';
-//        return "Car{" +
-//                "model='" + model + '\'' +
-//                ", series='" + series + '\'' +
-//                ", user='" + user + '\'' +
-//                ", year=" + year +
-//                '}';
     }
 
     @Override
